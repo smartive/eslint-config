c1dfc69d9afafffed7c0624aa0c9deae6bfaf3c0
Robert Vogt <robert@smartive.ch>
2022-02-23 17:37:09 +0100

    Add prettier

--- .eslintrc.react.js
+++ .eslintrc.react.js
@@ -1,21 +1,21 @@
 module.exports = {
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": 0,
-    "react/display-name": 0,
-    "react/forbid-component-props": ["warn", { forbid: ["style", "className"] }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 0,
+    'react/display-name': 0,
+    'react/forbid-component-props': ['warn', { forbid: ['style', 'className'] }],
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
   },
-  extends: ["./index", "plugin:react/recommended"],
+  extends: ['./index', 'plugin:react/recommended'],
   settings: {
     react: {
-      pragma: "React",
-      version: "detect",
+      pragma: 'React',
+      version: 'detect',
     },
   },
-  plugins: ["react", "react-hooks"],
+  plugins: ['react', 'react-hooks'],
 };
